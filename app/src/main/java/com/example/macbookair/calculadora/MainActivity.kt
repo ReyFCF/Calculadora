ff6329baca57878cb59d1ce5ceed8a6e0d02f820
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2018-08-23 18:04:55 -0500

    First commit

---
+++ app/src/main/java/com/example/macbookair/calculadora/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.macbookair.calculadora
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
